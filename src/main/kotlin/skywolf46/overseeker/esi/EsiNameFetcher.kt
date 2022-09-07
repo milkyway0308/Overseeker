db8244259191bfcbe810d2f7b480af1ed4b3ffe8
skywolf46 <skywolf461@gmail.com>
2022-09-07 09:23:46 +0900

    feat: add id fetcher

--- src/main/kotlin/skywolf46/overseeker/esi/EsiNameFetcher.kt
+++ src/main/kotlin/skywolf46/overseeker/esi/EsiNameFetcher.kt
@@ -5,6 +5,7 @@ import io.ktor.client.engine.cio.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import org.json.simple.JSONArray
+import org.json.simple.JSONObject
 import org.json.simple.parser.JSONParser
 import skywolf46.overseeker.util.fetch
 import skywolf46.overseeker.util.maybe
@@ -15,10 +16,12 @@ import kotlin.concurrent.write
 
 object EsiNameFetcher {
     private const val ESI_GET_NAME = "https://esi.evetech.net/latest/universe/names/?datasource=tranquility"
+    private const val ESI_GET_ID = "https://esi.evetech.net/latest/universe/ids/?datasource=tranquility"
     private val lock = ReentrantReadWriteLock()
     private val parser = JSONParser()
     private val client = HttpClient(CIO)
     private val cachedNames = mutableMapOf<Long, String?>()
+    private val cachedIds = mutableMapOf<String, Long>()
 
     fun getName(id: Long): String? {
         if (id == -1L)
@@ -45,7 +48,39 @@ object EsiNameFetcher {
             array.fetch().apply {
                 lock.write {
                     forEach {
+                        it.maybe<String>("name")?.apply {
+                            cachedIds[this] = it.of("id")
+                        }
                         cachedNames[it.of("id")] = it.maybe("name")
+                    }
+                }
+            }
+        }
+    }
+    suspend fun fetchIds(vararg id: String) {
+        val toResolve = lock.read {
+            id.filter { !cachedIds.containsKey(it)}
+        }.distinct()
+        if (toResolve.isEmpty())
+            return
+        client.post(ESI_GET_ID) {
+            setBody(JSONArray().apply {
+                addAll(toResolve)
+            }.toJSONString())
+        }.bodyAsText().apply {
+            val array = parser.parse(this)
+            if (array !is JSONObject)
+                return
+            lock.write {
+                // Outer for-each
+                array.forEach { k, arr ->
+                    if(arr !is JSONArray)
+                        return@forEach
+                    for(x in arr.fetch()) {
+                        x.maybe<Long>("id")?.apply {
+                            cachedIds[x.of("name")] = this
+                            cachedNames[this] = x.of("name")
+                        }
                     }
                 }
             }
